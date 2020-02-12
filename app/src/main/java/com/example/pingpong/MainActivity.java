90fa786788a5c5e90c4421d83ef6e50c7f649bc9
Daniel Morales <luisdanielmv@gmail.com>
2020-02-11 19:19:00 -0600

    Main-Menu - WIP - Create Green Menu Layout # Add pong font

--- app/src/main/java/com/example/pingpong/MainActivity.java
+++ app/src/main/java/com/example/pingpong/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.pingpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "Potato";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu);
+    }
+
+    public void buttonTapHandler(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        EditText editText = (EditText) findViewById(R.id.txtTitle);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
