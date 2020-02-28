8315d7a04a2e0024fa68114642783d7dd2de28bb
Daniel Morales <luisdanielmv@gmail.com>
2020-02-27 18:37:13 -0600

    pong theme lost main menu changes

--- app/src/main/java/com/example/pingpong/MainActivity.java
+++ app/src/main/java/com/example/pingpong/MainActivity.java
@@ -1,25 +1,46 @@
 package com.example.pingpong;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.pingpong.activities.PongActivity;
-import com.example.pingpong.views.PongView;
+import com.example.pingpong.activities.SecondActivity;
 
 public class MainActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.DarkTheme);
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main_menu);
+        startActivity(new Intent(getBaseContext(), SecondActivity.class));
+    }
 
+    public void buttonTapHandler(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        // startActivity(intent);
         startActivity(new Intent(getBaseContext(), PongActivity.class));
 
+    }
+
+    public void switchTapHandler(Switch view) {
+
+        view.toggle();
+
+        setTheme(R.style.DarkTheme);
+
+
+        Log.d("Theme", getTheme().toString());
+
+        finish();
+        recreate();
     }
 
     // This method executes when the player starts the game
